c8cee096146871421851382e314db3c7bfc0a5c6
r0cc4t0 <guilherme.roccato@outlook.com>
2021-07-05 23:31:36 -0300

    Supply Search

---
+++ lib/inmana/supplies/show.ex
@@ -0,0 +1,19 @@
+defmodule Inmana.Supplies.Show do
+  alias Inmana.{Repo, Supply}
+
+  def call(uuid) do
+    # Supply
+    # |> Repo.get(uuid)
+    # |> handle_get()
+    case Repo.get(Supply, uuid) do
+      nil ->
+        {:error, %{result: "Supply not found!", status: :not_found}}
+
+      supply ->
+        {:ok, supply}
+    end
+  end
+
+  # defp handle_get(%Supply{} = result), do: {:ok, result}
+  # defp handle_get(nil), do: {:error, %{result: "Supply not found!", status: :not_found}}
+end
